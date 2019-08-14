7a45191388c98163461088127c16af26df049d2a
hongqima <hongqima@yangqianguan.com>
2019-08-14 17:28:07 +0800

    listView

--- githubflutter/lib/main.dart
+++ githubflutter/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'strings.dart';
 
-void main() => runApp(new GHFlutterApp());
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+void main() => runApp(new GHFlutterApp());
 
 class GHFlutterApp extends StatelessWidget {
   @override
@@ -15,16 +17,48 @@ class GHFlutterApp extends StatelessWidget {
 }
 
 class GHFlutterState extends State<GHFlutter> {
+  var _members = [];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
-	Widget build(BuildContext context) {
-  	var scaffold = new Scaffold (
-    	appBar: new AppBar(
-      	title: new Text(Strings.appTitle),
-    	),
-    	body: new Text(Strings.appTitle),
-  	);
-  	return scaffold;
-	}
+  Widget build(BuildContext context) {
+    var scaffold = new Scaffold(
+      appBar: new AppBar(
+        title: new Text(Strings.appTitle),
+      ),
+      body: new ListView.builder(
+          itemCount: _members.length * 2,
+          itemBuilder: (BuildContext context, int position) {
+            if (position.isOdd) return new Divider();
+
+            final index = position ~/ 2;
+
+            return _buildRow(index);
+          }),
+    );
+    return scaffold;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  _loadData() async {
+    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      _members = json.decode(response.body);
+    });
+  }
+
+  Widget _buildRow(int i) {
+    return new Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: new ListTile(
+            title: new Text("${_members[i]["login"]}", style: _biggerFont)));
+  }
 }
 
 class GHFlutter extends StatefulWidget {
